497c52cecd1d8dfcbb714295e6bbfe3378789125
Simone Westphal <contact@swestphal.net>
2018-03-11 19:09:00 +0100

    feat: js get data

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
+
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
-     console.log("ready");
+    //event listener 
+     
+    const get = document.getElementById('get');
 
-    }
-  };
+
+
+    //events
+
+    get.addEventListener('click', getData);
+    
+    
+
+    function getData() {
+        var xhr = new XMLHttpRequest();
+        xhr.open ('GET',"https://api.myjson.com/bins/mke4l");
+        xhr.onload = function() {
+            if (xhr.status ===200 && xhr.readyState==4) {
+                console.log(JSON.parse(xhr.responseText));
+            }
+            else {
+                console.log("request failed. status: "+xhr.status);
+            }
+        };
+        xhr.send();
+    }   
+}}
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <input type="text" name="name" id="name" value="tester">
+    <button name="get" id="get">Get Data</button>
 <script
   src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
   integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
