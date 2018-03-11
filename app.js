9383d4440f3366212894de9b11c8e0d768ca3351
Simone Westphal <contact@swestphal.net>
2018-03-11 17:51:14 +0100

    feat: included js file

---
+++ app.js
@@ -0,0 +1,6 @@
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+     console.log("ready");
+
+    }
+  };
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web-API</title>
+</head>
+<body>
+    <input type="text" name="name" id="name" value="tester">
+<script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+  crossorigin="anonymous"></script>
+  <script src="app.js"></script>
+</body>
+</html>
