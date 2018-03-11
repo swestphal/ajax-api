9eeaaa0b6d5cf4489a3330c9acf1711b6673daf8
Simone Westphal <contact@swestphal.net>
2018-03-11 20:38:34 +0100

    feat: response working

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
+/**
+ * Make a X-Domain request to url and callback.
+ *
+ * @param url {String}
+ * @param method {String} HTTP verb ('GET', 'POST', 'DELETE', etc.)
+ * @param data {String} request body
+ * @param callback {Function} to callback on completion
+ * @param errback {Function} to callback on error
+ */
 
-document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-    //event listener 
-     
-    const get = document.getElementById('get');
+ //let data = {id:"2",name:"simone",company:"thinkweb",cost:"null"};
+ var data = {xid: "1", name: "Marie", company: "thinkweb", cost: "zero"};
+console.log(data);
+
+xdr('http://web-api.swestphal.net/check.php','POST',data,onDone,onError);
+
+//xdr('http://web-api.swestphal.net/check.php','GET',data,onDone,onError);
+
+function onDone() {
+    console.log("ok");
+}
+
+function onError() {
+    console.log("fail");
+}
 
 
-
-    //events
-
-    get.addEventListener('click', getData);
+function xdr(url, method, data, callback, errback) {
+    var req;
     
-    
+    if(XMLHttpRequest) {
+        req = new XMLHttpRequest();
 
-    function getData() {
-        var xhr = new XMLHttpRequest();
-        xhr.open ('GET',"https://api.myjson.com/bins/mke4l");
-        xhr.onload = function() {
-            if (xhr.status ===200 && xhr.readyState==4) {
-                console.log(JSON.parse(xhr.responseText));
-            }
-            else {
-                console.log("request failed. status: "+xhr.status);
-            }
+        if('withCredentials' in req) {
+            req.open(method, url, true);
+            req.onerror = errback;
+            req.onreadystatechange = function() {
+                if (req.readyState === 4) {
+                    if (req.status >= 200 && req.status < 400) {
+                        callback(req.responseText);
+                    } else {
+                        errback(new Error('Response returned with non-OK status'));
+                    }
+                }
+            };
+            req.send(data);
+        }
+    } else if(XDomainRequest) {
+        req = new XDomainRequest();
+        req.open(method, url);
+        req.onerror = errback;
+        req.onload = function() {
+            callback(req.responseText);
         };
-        xhr.send();
-    }   
-}}
+        req.send(data);
+    } else {
+        errback(new Error('CORS not supported'));
+    }
+}
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
     <input type="text" name="name" id="name" value="tester">
     <button name="get" id="get">Get Data</button>
+    <button name="send" id="send">Send Data</button>
 <script
   src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
   integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
